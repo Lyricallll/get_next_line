d0df4de52a73578124d617d781da520796c2da19
lyricall <lachariotte30@gmail.com>
2024-11-25 16:33:20 +0100

    mal a la tete

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,38 @@
 /*   By: agraille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 09:01:26 by agraille          #+#    #+#             */
-/*   Updated: 2024/11/25 11:45:23 by agraille         ###   ########.fr       */
+/*   Updated: 2024/11/25 15:49:36 by agraille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "fcntl.h"
 
-char	*get_next_line(int fd)
+size_t	get_next_line(int fd)
 {
-	if (!fd)
-		return (NULL);
+	char	*buffer;
+	size_t	oc_read;
+
+	if (fd < 0)
+		return (0);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (0);
+	oc_read = read(fd, buffer, BUFFER_SIZE);
+	if (oc_read == -1)
+		return (0);
+	free(buffer);
+	return (oc_read);
+}
+int	main (void)
+{
+    int fd = open("test.txt", O_RDONLY);
+    size_t oc_read;
+	
+    while ((oc_read = get_next_line(fd)) > 0)
+    {
+        printf("Bytes read: %zu\n", oc_read);
+    }
+    close(fd);
+    return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,23 @@
 /*   By: agraille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 09:01:25 by agraille          #+#    #+#             */
-/*   Updated: 2024/11/25 09:59:35 by agraille         ###   ########.fr       */
+/*   Updated: 2024/11/25 15:45:19 by agraille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
 
-char	*get_next_line(int fd);
+# endif
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "fcntl.h"
+
+// char	*get_next_line(int fd);
 
 #endif
